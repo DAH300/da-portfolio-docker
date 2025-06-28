38cf06fcad2a19c9772880398cacdf615ed53ea2
DAH300 <DHowell300@Comcast.net>
2025-06-28 15:53:39 -0400

    Added projects.ts and socials.ts
    Created files that contains all projects and all socials to simplify the page.tsx file and more easily add/remove/modify projects or socials in the future

---
+++ src/data/projects.ts
@@ -0,0 +1,24 @@
+// data/projects.ts
+
+const projects = [
+    {
+        href: "https://github.com/DAH300/da-portfolio-docker",
+        title: "Portfolio Site (This!)",
+        description:
+            "Full-stack personal site built with Next.js 15, Tailwind CSS, and Docker for consistent development. Features live reloading.",
+    },
+    {
+        href: "https://github.com/DAH300",
+        title: "Embedded Temp Monitor (PIC)",
+        description:
+            "Reads DS18B20 sensor via 1-Wire and displays real-time temperature using an I2C LCD on a PIC16F877A. Timers, interrupts, and bit-banging used extensively.",
+    },
+    {
+        href: "https://github.com/DAH300/Bills_App",
+        title: "Self-Updating Python App",
+        description:
+            "A Python application packaged as a `.exe` that checks GitHub for updates and relaunches itself when a new version is available. Built with PyInstaller and GitHub Releases API.",
+    },
+];
+
+export default projects;
---
+++ src/data/socials.ts
@@ -0,0 +1,25 @@
+// data/socials.ts
+
+import GitHubIcon from '@/components/icons/GitHubIcon';
+import LinkedInIcon from '@/components/icons/LinkedInIcon';
+import LeetCodeIcon from '@/components/icons/LeetCodeIcon';
+
+const socials = [
+    {
+        name: 'GitHub',
+        href: 'https://github.com/DAH300',
+        Icon: GitHubIcon,
+    },
+    {
+        name: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/dwayne-howell-461b4a217/',
+        Icon: LinkedInIcon,
+    },
+    {
+        name: 'LeetCode',
+        href: 'https://leetcode.com/N997CYUOst',
+        Icon: LeetCodeIcon,
+    },
+];
+
+export default socials;
